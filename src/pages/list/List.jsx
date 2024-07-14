643b53af1560f1f38b5acb33c03f12512ba40d95
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-07-14 17:24:40 -0500

    feat: :sparkles: grid de resultados de la PLP

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const List = () => {
   const location = useLocation();
@@ -76,7 +77,15 @@ const List = () => {
             <button>Search</button>
           </div>
           <div className="listResult">
-
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
           </div>
         </div>
       </div>
